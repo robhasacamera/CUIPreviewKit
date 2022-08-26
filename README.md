13e749d822fc16fc668c14283463aa7fb5017547
Robert Cole <robhasacamera@gmail.com>
2022-08-25 19:17:38 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CUIPreviewKit
+
+A description of this package.
---
+++ Tests/CUIPreviewKitTests/CUIPreviewKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CUIPreviewKit
+
+final class CUIPreviewKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(CUIPreviewKit().text, "Hello, World!")
+    }
+}
